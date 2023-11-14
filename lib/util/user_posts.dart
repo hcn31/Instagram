a5378196ad0b94d07e6799dbc4bb25723bccbb71
hcn31 <saidihocuine913@gmail.com>
2023-11-14 18:36:47 +0100

    adding a color to favorite icon

--- lib/util/user_posts.dart
+++ lib/util/user_posts.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
 
-class UserPosts extends StatelessWidget {
-  final List<Color> colorr = [Colors.grey, Colors.red];
-  int indexColor = 0;
+class UserPosts extends StatefulWidget {
   final String name;
   UserPosts({required this.name});
+
+  @override
+  State<UserPosts> createState() => _UserPostsState();
+}
+
+class _UserPostsState extends State<UserPosts> {
+  final List<Color> colorr = [Colors.red, Colors.grey];
+
+  int indexColor = 0;
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -26,7 +34,7 @@ class UserPosts extends StatelessWidget {
                       ),
                       const SizedBox(width: 10.0),
                       Text(
-                        name,
+                        widget.name,
                         style: TextStyle(fontWeight: FontWeight.bold),
                       ),
                     ],
@@ -34,30 +42,28 @@ class UserPosts extends StatelessWidget {
                   Icon(Icons.menu)
                 ])),
         Container(
-            height: 400,
-            color: Colors.grey,
-            child: Text(
-              '$indexColor',
-              style: TextStyle(color: Colors.red),
-            )),
+          height: 400,
+          color: Colors.grey,
+        ),
         Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
             Row(
               children: [
                 IconButton(
-                  icon: const Icon(Icons.favorite),
+                  icon: Icon(Icons.favorite, color: colorr[indexColor]),
                   onPressed: () {
-                    setState() {
-                      if (indexColor == 0) indexColor = 1;
-                      indexColor = 0;
-                    }
+                    setState(() {
+                      if (indexColor == 0)
+                        indexColor = 1;
+                      else
+                        indexColor = 0;
+                    });
                   },
                 ),
                 Padding(
                   padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
-                  child: Icon(Icons.chat_bubble_outline,
-                      color: colorr[indexColor]),
+                  child: Icon(Icons.chat_bubble_outline),
                 ),
                 Icon(Icons.share),
               ],
@@ -80,7 +86,8 @@ class UserPosts extends StatelessWidget {
             child: RichText(
               text: TextSpan(style: TextStyle(color: Colors.black), children: [
                 TextSpan(
-                    text: name, style: TextStyle(fontWeight: FontWeight.bold)),
+                    text: widget.name,
+                    style: TextStyle(fontWeight: FontWeight.bold)),
                 TextSpan(
                     text:
                         " today i ve graduated from my school soo am soo gappy to anounce that and share it with you")
